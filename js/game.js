8969990d43ed7b72758bc7c79b80f22dc5ca3837
ptnguyen617 <ptnguyen617@yahoo.com>
2022-01-26 21:49:32 -0500

    Creates game function

---
+++ js/game.js
@@ -0,0 +1,53 @@
+//creates object and assign to game
+let GameManager = {
+    setGameStart: function(classType){
+        this.resetPlayer(classType);
+        this.setPreFight();
+    },
+    
+    resetPlayer: function(classType){
+        switch (classType){//switch condition
+            case  "Johnny":
+                player = new Player(classType, 200, 0 , 200, 100, 50);
+                break;
+            case  "Subzero":
+                player = new Player(classType, 100, 0 , 100, 150, 200);
+                break;
+            case  "Scorpion":
+                player = new Player(classType, 250, 0 , 150, 200, 50);
+                break;
+            case  "Raiden":
+                player = new Player(classType, 200, 0 , 50, 200, 100);
+                break;
+        }
+        let getInterFace =  document.querySelector(".interface");
+        getInterFace.innerHTML = '<img src="imgs/"' + classType.toLowerCase() + '.png" class="imgs-avatar"/><div><h3> ' + classType + '</h3><p>Health:  ' + player.health + '</p><p>Mana:  ' + player.mana+ '</p><p>Power:  ' + player.power + '</p><p>Agility :  ' + player.agility + '</p><p>Speed:  ' + player.speed + '</p></div>';
+ },
+    setPreFight: function(){
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getArena = document.querySelector(".arena");
+        getHeader.innerHTML = '<p>Task: Pick your enemy!</p>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Pick Your Enemy!</a>';
+        getArena.style.visibility = "visible";
+    },
+    setFight: function(){
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
+        //Create Enemy Fighter!
+        let enemy00 = new Enemy("Johnny", 100, 0, 50, 100, 100);
+        let enemy01 = new Enemy("Sub-Zero", 200, 0, 150, 80, 100);
+        let chooseRandomEnemy = Math.floor(Math.random()* Math.floor(2));
+        console.log(chooseRandomEnemy);
+        switch(chooseRandomEnemy){
+            case 0:
+                enemy = enemy00;
+                break;
+            case 1:
+                enemy = enemy01;
+                break;
+        }
+
+    }
+}
---
+++ js/player.js
@@ -0,0 +1,12 @@
+
+let player;
+
+//create function and property inside Player function
+function Player(classType, health, mana, power, agility, speed){
+    this.classType = this.classType;
+    this.health = health;
+    this.mana = mana;
+    this.power = power;
+    this.agility = agility;
+    this.speed =  speed;
+}
